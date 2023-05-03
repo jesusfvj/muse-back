113e4ae461289b776cabe6536faf9503fa08907f
Lander Sola <landersolaaguirre@gmail.com>
2023-05-03 13:07:42 +0200

    login function without TOKEN

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const userRouter = express.Router();
-const { register } = require("../controllers/user");
+const { register, logInUser } = require("../controllers/user");
 
 userRouter.post("/register", register);
-// userRouter.post("/login", logInUser);
+userRouter.post("/login", logInUser);
 // userRouter.delete("/delete", deleteUser);
 
 module.exports = userRouter;

9a866d05b8d0731980adcecf7652be426e57cf2f
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-05-04 12:24:14 +0200

    add .env.example and  cloudinary first steps

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const userRouter = require("./routes/userRoutes");
+const uploadSongsRouter = require("./routes/uploadSongsRoutes");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : './uploads/'
+}));
+
 app.use('/user', userRouter)
+app.use('/uploadsongs', uploadSongsRouter)
 
 module.exports = app;

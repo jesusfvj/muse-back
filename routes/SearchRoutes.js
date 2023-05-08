7f3eae86621d8afe90843bc129caf71f04a7ef6e
Miquel <abellagarciamiquel@gmail.com>
2023-05-08 09:17:40 +0200

    fixed search route

--- routes/SearchRoutes.js
+++ routes/SearchRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { search } = require("../controllers/search");
 const searchRouter = express.Router();
 
-searchRouter.get("/:query", search);
+searchRouter.get("/:query/:id", search);
 
 module.exports = searchRouter;

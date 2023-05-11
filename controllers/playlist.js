c5a8c8f12f9a03be8825a52cf09a5a10a62c99cf
Miquel <abellagarciamiquel@gmail.com>
2023-05-11 13:13:49 +0200

    deleted playlistsbyuserid

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,14 +1,12 @@
 const Playlist = require("../models/Playlist");
 const User = require("../models/User");
-const {
-  uploadImage
-} = require("../utils/cloudinary");
-const fs = require('fs-extra');
+const { uploadImage } = require("../utils/cloudinary");
+const fs = require("fs-extra");
 
 const getPlaylists = async (req, res) => {
   try {
     const playlists = await Playlist.find({
-      isPrivate: false
+      isPrivate: false,
     });
 
     return res.status(200).json({
@@ -25,34 +23,33 @@ const getPlaylists = async (req, res) => {
 };
 
 const followPlaylists = async (req, res) => {
-  const {
-    loggedUserId,
-    playlistId,
-    isAdded
-  } = req.body;
+  const { loggedUserId, playlistId, isAdded } = req.body;
 
   try {
     const loggedUser = await User.findOne({
-      _id: loggedUserId
+      _id: loggedUserId,
     });
     if (isAdded) {
       await loggedUser.updateOne({
         $addToSet: {
           followedPlaylists: {
-            $each: [playlistId]
-          }
+            $each: [playlistId],
+          },
         },
       });
 
-      await Playlist.findOneAndUpdate({
-        _id: playlistId
-      }, {
-        $addToSet: {
-          followedBy: {
-            $each: [loggedUserId]
-          }
+      await Playlist.findOneAndUpdate(
+        {
+          _id: playlistId,
+        },
+        {
+          $addToSet: {
+            followedBy: {
+              $each: [loggedUserId],
+            },
+          },
         }
-      });
+      );
 
       return res.status(200).json({
         ok: true,
@@ -64,20 +61,23 @@ const followPlaylists = async (req, res) => {
       await loggedUser.updateOne({
         $pull: {
           followedPlaylists: {
-            $in: playlistId
-          }
+            $in: playlistId,
+          },
         },
       });
 
-      await Playlist.findOneAndUpdate({
-        _id: playlistId
-      }, {
-        $pull: {
-          followedBy: {
-            $in: loggedUserId
-          }
+      await Playlist.findOneAndUpdate(
+        {
+          _id: playlistId,
+        },
+        {
+          $pull: {
+            followedBy: {
+              $in: loggedUserId,
+            },
+          },
         }
-      });
+      );
       return res.status(200).json({
         ok: true,
         loggedUserId,
@@ -95,13 +95,10 @@ const followPlaylists = async (req, res) => {
 };
 
 const createPlaylist = async (req, res) => {
-  const {
-    name,
-    isPrivate
-  } = JSON.parse(req.body.imagePlaylistData);
+  const { name, isPrivate } = JSON.parse(req.body.imagePlaylistData);
 
-  const userId = req.params.userId
-  const file = req.files[0]
+  const userId = req.params.userId;
+  const file = req.files[0];
 
   try {
     if (!file) {
@@ -112,25 +109,25 @@ const createPlaylist = async (req, res) => {
     }
     if (file) {
       const loggedUser = await User.findOne({
-        _id: userId
+        _id: userId,
       });
       const newPlaylist = new Playlist({
         name: name,
         user: userId,
-        isPrivate: isPrivate
+        isPrivate: isPrivate,
       });
       //Upload thumbnail to Cloudinary
-      const resultImage = await uploadImage(file.path)
-      newPlaylist.color = resultImage.colors[0][0]
-      newPlaylist.thumbnail = resultImage.secure_url
-      newPlaylist.thumbnailCloudinaryId = resultImage.public_id
+      const resultImage = await uploadImage(file.path);
+      newPlaylist.color = resultImage.colors[0][0];
+      newPlaylist.thumbnail = resultImage.secure_url;
+      newPlaylist.thumbnailCloudinaryId = resultImage.public_id;
       await newPlaylist.save();
       await loggedUser.updateOne({
         $push: {
-          playlists: newPlaylist._id
-        }
+          playlists: newPlaylist._id,
+        },
       });
-      await fs.unlink(file.path)
+      await fs.unlink(file.path);
       return res.status(201).json({
         ok: true,
         newPlaylist,
@@ -146,17 +143,14 @@ const createPlaylist = async (req, res) => {
 };
 
 const deletePlaylist = async (req, res) => {
-  const {
-    loggedUserId,
-    playlistId
-  } = req.body;
+  const { loggedUserId, playlistId } = req.body;
 
   try {
     const loggedUser = await User.findOne({
-      _id: loggedUserId
+      _id: loggedUserId,
     });
     const playlistToDelete = await Playlist.findOne({
-      _id: playlistId
+      _id: playlistId,
     });
     if (playlistToDelete.user.toString() !== loggedUserId) {
       return res.status(401).json({
@@ -166,8 +160,8 @@ const deletePlaylist = async (req, res) => {
     }
     await loggedUser.updateOne({
       $pull: {
-        playlists: playlistId
-      }
+        playlists: playlistId,
+      },
     });
     await Playlist.findByIdAndDelete(playlistId).then((deletedPlaylist) => {
       return res.status(200).json({
@@ -175,13 +169,16 @@ const deletePlaylist = async (req, res) => {
         deletedPlaylist,
       });
     });
-    await User.updateMany({
-      followedPlaylists: playlistId
-    }, {
-      $pull: {
-        followedPlaylists: playlistId
+    await User.updateMany(
+      {
+        followedPlaylists: playlistId,
+      },
+      {
+        $pull: {
+          followedPlaylists: playlistId,
+        },
       }
-    });
+    );
   } catch (error) {
     console.log(error);
     return res.status(503).json({
@@ -192,15 +189,10 @@ const deletePlaylist = async (req, res) => {
 };
 
 const updatePlaylist = async (req, res) => {
-  const {
-    loggedUserId,
-    playlistId,
-    newName,
-    thumbnailUrl
-  } = req.body;
+  const { loggedUserId, playlistId, newName, thumbnailUrl } = req.body;
   try {
     const playlistToUpdate = await Playlist.findOne({
-      _id: playlistId
+      _id: playlistId,
     });
     if (playlistToUpdate.user.toString() !== loggedUserId) {
       return res.status(401).json({
@@ -210,10 +202,10 @@ const updatePlaylist = async (req, res) => {
     }
     const oldName = playlistToUpdate.name;
     await playlistToUpdate.updateOne({
-      name: newName
+      name: newName,
     });
     await playlistToUpdate.updateOne({
-      thumbnail: thumbnailUrl
+      thumbnail: thumbnailUrl,
     });
     return res.status(200).json({
       ok: true,
@@ -230,9 +222,7 @@ const updatePlaylist = async (req, res) => {
 };
 
 const getPlaylistById = async (req, res) => {
-  const {
-    id
-  } = req.params;
+  const { id } = req.params;
 
   if (id.length !== 24) {
     return res.status(200).json({
@@ -242,7 +232,7 @@ const getPlaylistById = async (req, res) => {
 
   try {
     const playlist = await Playlist.findOne({
-      _id: id
+      _id: id,
     }).populate("tracks");
 
     if (!playlist) {
@@ -266,15 +256,11 @@ const getPlaylistById = async (req, res) => {
 };
 
 const isPrivate = async (req, res) => {
-  const {
-    loggedUserId,
-    playlistId,
-    isPrivate
-  } = req.body;
+  const { loggedUserId, playlistId, isPrivate } = req.body;
 
   try {
     const playlistToUpdate = await Playlist.findOne({
-      _id: playlistId
+      _id: playlistId,
     });
 
     if (playlistToUpdate.user.toString() !== loggedUserId) {
@@ -284,17 +270,20 @@ const isPrivate = async (req, res) => {
       });
     }
     await playlistToUpdate.updateOne({
-      isPrivate: !isPrivate
+      isPrivate: !isPrivate,
     });
 
     if (!isPrivate) {
-      await User.updateMany({
-        followedPlaylists: playlistId
-      }, {
-        $pull: {
-          followedPlaylists: playlistId
+      await User.updateMany(
+        {
+          followedPlaylists: playlistId,
+        },
+        {
+          $pull: {
+            followedPlaylists: playlistId,
+          },
         }
-      });
+      );
     }
 
     return res.status(200).json({
@@ -309,39 +298,34 @@ const isPrivate = async (req, res) => {
   }
 };
 const addTracks = async (req, res) => {
-  const {
-    loggedUserId,
-    playlistId,
-    trackId,
-    isAdded
-  } = req.body
+  const { loggedUserId, playlistId, trackId, isAdded } = req.body;
   try {
     const playlistToUpdate = await Playlist.findOne({
-      _id: playlistId
+      _id: playlistId,
     });
     if (!playlistToUpdate) {
       return res.status(401).json({
         ok: false,
         message: "Error, this playlist doesn't exist",
-      })
+      });
     }
     if (playlistToUpdate.user.toString() !== loggedUserId) {
       return res.status(401).json({
         ok: false,
         message: "You are not the owner of this playlist",
-      })
+      });
     }
     await playlistToUpdate.updateOne({
       $addToSet: {
         tracks: {
-          $each: trackId
-        }
-      }
+          $each: trackId,
+        },
+      },
     });
     return res.status(200).json({
       ok: true,
       playlistId,
-      addedTracks: trackId
+      addedTracks: trackId,
     });
   } catch (error) {
     return res.status(503).json({
@@ -349,15 +333,15 @@ const addTracks = async (req, res) => {
       msg: "Oops, something happened",
     });
   }
-}
+};
 
 const duplicatePlaylist = async (req, res) => {
-  const { loggedUserId, playlistId } = req.body
+  const { loggedUserId, playlistId } = req.body;
   try {
     const playlistToDuplicate = await Playlist.findOne({
-      _id: playlistId
+      _id: playlistId,
     });
-    const loggedUser = await User.findById(loggedUserId).populate('playlists');
+    const loggedUser = await User.findById(loggedUserId).populate("playlists");
     if (playlistToDuplicate.user.toString() === loggedUserId) {
       return res.status(400).json({
         ok: false,
@@ -371,19 +355,24 @@ const duplicatePlaylist = async (req, res) => {
       });
     }
     const userPlaylists = loggedUser.playlists;
-    const foundPlaylist = userPlaylists.some(playlist => playlist.copyFrom.toString().includes(playlistId));
+    const foundPlaylist = userPlaylists.some((playlist) =>
+      playlist.copyFrom.toString().includes(playlistId)
+    );
     if (foundPlaylist) {
-
-      const playlist = userPlaylists.find(playlist => playlist.copyFrom.toString() === playlistId);
-      const duplicatedTracksIds = playlist.tracks.map(track => track);
-      const missingTracks = playlistToDuplicate.tracks.filter(track => !duplicatedTracksIds.includes(track));
+      const playlist = userPlaylists.find(
+        (playlist) => playlist.copyFrom.toString() === playlistId
+      );
+      const duplicatedTracksIds = playlist.tracks.map((track) => track);
+      const missingTracks = playlistToDuplicate.tracks.filter(
+        (track) => !duplicatedTracksIds.includes(track)
+      );
 
       if (missingTracks.length !== 0) {
         playlist.updateOne({ $push: { tracks: { $each: missingTracks } } });
         return res.status(200).json({
           ok: true,
           msg: "This playlist is already in your library. The missing songs have been added to the corresponding playlist.",
-          missingTracks
+          missingTracks,
         });
       }
       return res.status(200).json({
@@ -391,7 +380,7 @@ const duplicatePlaylist = async (req, res) => {
         msg: "This playlist has already been duplicated",
         playlist: playlist.tracks,
         playlistToDuplicate: playlistToDuplicate.tracks,
-        missingTracks
+        missingTracks,
       });
     }
     const newPlaylist = new Playlist({
@@ -399,23 +388,24 @@ const duplicatePlaylist = async (req, res) => {
       user: loggedUserId,
       thumbnail: playlistToDuplicate.thumbnail,
       tracks: playlistToDuplicate.tracks,
-      copyFrom: playlistId
+      copyFrom: playlistId,
     });
     await newPlaylist.save();
     await loggedUser.updateOne({ $push: { playlists: newPlaylist._id } });
     return res.status(201).json({
       ok: true,
       newPlaylist,
-      loggedUser: loggedUser.playlists
+      loggedUser: loggedUser.playlists,
     });
   } catch (error) {
     return res.status(400).json({
       ok: false,
       msg: "Oops, something happened",
-      error
+      error,
     });
   }
-}
+};
+
 module.exports = {
   getPlaylists,
   getPlaylistById,
@@ -425,5 +415,5 @@ module.exports = {
   createPlaylist,
   isPrivate,
   duplicatePlaylist,
-  addTracks
+  addTracks,
 };

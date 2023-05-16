05686512417c64bc1b70de45147da90e3713386c
Lander Sola <landersolaaguirre@gmail.com>
2023-05-16 12:02:21 +0200

    Stripe

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const trackRouter = require("./routes/TrackRoutes");
 const searchRouter = require("./routes/SearchRoutes");
 const queueRouter = require("./routes/PlayQueue");
 const albumRouter = require("./routes/AlbumRoutes");
+const StripeRouter = require("./routes/StripeRoutes");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
@@ -21,5 +22,6 @@ app.use("/track", trackRouter);
 app.use("/search", searchRouter);
 app.use("/queue", queueRouter);
 app.use("/album", albumRouter);
+app.use("/stripe", StripeRouter);
 
 module.exports = app;

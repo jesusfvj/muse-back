897bd2c27a7ea5f47be3857e676d72b619c0f8ec
Miquel <abellagarciamiquel@gmail.com>
2023-05-18 15:15:50 +0200

    push to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { dbConnection } = require("./database/config");
 dbConnection().then(async function onServerInit() {
   console.log("DB connected");
 
-  app.listen(Config.app.PORT, () => {
+  app.listen(4000, () => {
     console.log(`Serving on port ${Config.app.PORT}`);
   });
 });

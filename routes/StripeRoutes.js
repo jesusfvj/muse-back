fe8533cec8db7c7eaa8ecc1bbedfcd0f5a7cd835
Miquel <abellagarciamiquel@gmail.com>
2023-05-19 09:22:18 +0200

    fixed username route

--- routes/StripeRoutes.js
+++ routes/StripeRoutes.js
@@ -4,7 +4,7 @@ const checkJWT = require("../middlewares/checkJWT");
 const stripeRouter = express.Router();
 const stripe = require("stripe")('pk_test_51N80saK8GYiDB5U2dA7gHIgSiGYxbueVVFSsP5W0c4mTIyr2oPqUiYpeW46ijdYrC4MWUNFIIDKAP06hC9IAsY0400tBSO8Ghc');
 
-stripeRouter.get("/setStripe", checkJWT, setStripe);
-stripeRouter.post("/stripePayment", checkJWT, stripePayment);
+stripeRouter.get("/setStripe", setStripe);
+stripeRouter.post("/stripePayment", stripePayment);
 
 module.exports = stripeRouter; 
